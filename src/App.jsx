088e185e6fb145291d9bb6217bc490beee885e85
Yakumaa <maharjanshrish8@gmail.com>
2024-05-20 10:15:21 +0545

    5.1: Refactor App component for login

--- src/App.jsx
+++ src/App.jsx
@@ -67,12 +67,12 @@ const App = () => {
         loginForm() :
         <div>
           <p>{user.name} logged-in</p>
+
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
         </div>
       }
-
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
     </div>
   )
 }
