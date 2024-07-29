2bc8e04915618e7b37a8f07f6e89e2b93ed5b686
Yakumaa <maharjanshrish8@gmail.com>
2024-07-29 23:44:27 +0545

    5.5: Refactor Togglable component to use forwardRef and useImperativeHandle

--- src/App.jsx
+++ src/App.jsx
@@ -1,144 +1,160 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import BlogForm from './components/BlogForm'
 import LoginForm from './components/LoginForm'
+import Togglable from './components/Togglable'
 
 const Notification = ({ message, type }) => {
-  if (message === null) {
-    return null
-  }
+	if (message === null) {
+		return null
+	}
 
-  let className = 'success'
-  if (type === 'error') {
-    className = 'error'
-  }
+	let className = 'success'
+	if (type === 'error') {
+		className = 'error'
+	}
 
-  return (
-    <>
-    <div className={className}>
-      {message}
-    </div>
-    </>
-  )
+	return (
+		<>
+			<div className={className}>{message}</div>
+		</>
+	)
 }
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
-  const [message, setMessage] = useState(null)
-  const [messageType, setMessageType] = useState('')
-  const [loginVisible, setLoginVisible] = useState(false)
+	const [blogs, setBlogs] = useState([])
+	const [title, setTitle] = useState('')
+	const [author, setAuthor] = useState('')
+	const [url, setUrl] = useState('')
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+	const [user, setUser] = useState(null)
+	const [message, setMessage] = useState(null)
+	const [messageType, setMessageType] = useState('')
+	const [loginVisible, setLoginVisible] = useState(false)
 
-  useEffect(() => {
-    blogService.getAll().then(blogs => {
-      console.log(blogs)
-      setBlogs( blogs )
-    })  
-  }, [])
+	useEffect(() => {
+		blogService.getAll().then((blogs) => {
+			console.log(blogs)
+			setBlogs(blogs)
+		})
+	}, [])
 
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      console.log(user)
-      setUser(user)
-      blogService.setToken(user.token)
-    }
-  }, [])
+	useEffect(() => {
+		const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+		if (loggedUserJSON) {
+			const user = JSON.parse(loggedUserJSON)
+			console.log(user)
+			setUser(user)
+			blogService.setToken(user.token)
+		}
+	}, [])
 
-  const addBlog = async (blogObject) => {
-    try {
-      const returnedBlog = await blogService.create(blogObject);
-      setBlogs(blogs.concat(returnedBlog));
-      setMessage(`A new blog ${returnedBlog.title} by ${returnedBlog.author} added`)
-      setMessageType('success')
-      setTimeout(() => {
-        setMessage(null)
-      }, 5000)
-    } catch (exception) {
-      setMessage(`Error adding blog: ${exception}`)
-      setMessageType('error')
-      setTimeout(() => {
-        setMessage(null)
-      }, 5000)
-    }
-  };
+	const addBlog = (blogObject) => {
+		try {
+			blogFormRef.current.toggleVisibility()
+			blogService.create(blogObject).then((returnedBlog) => {
+				setBlogs(blogs.concat(returnedBlog))
+				setTitle('')
+				setAuthor('')
+				setUrl('')
+				setMessage(`A new blog ${returnedBlog.title} by ${returnedBlog.author} added`)
+				setMessageType('success')
+				setTimeout(() => {
+					setMessage(null)
+				}, 5000)
+			})
+		} catch (exception) {
+			setMessage(`Error adding blog: ${exception}`)
+			setMessageType('error')
+			setTimeout(() => {
+				setMessage(null)
+			}, 5000)
+		}
+	}
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
+	const handleLogin = async (event) => {
+		event.preventDefault()
 
-    try{
-      const user = await loginService.login({
-        username, password,
-      })
+		try {
+			const user = await loginService.login({
+				username,
+				password,
+			})
 
-      window.localStorage.setItem(
-        'loggedBlogappUser', JSON.stringify(user)
-      )
-      blogService.setToken(user.token)
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch(exception) {
-      console.log('Wrong credentials')
-      setMessage(`Wrong username or password`)
-      setMessageType('error')
-      setTimeout(() => {
-        setMessage(null)
-      }, 5000)
-    }
-  }
+			window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+			blogService.setToken(user.token)
+			setUser(user)
+			setUsername('')
+			setPassword('')
+		} catch (exception) {
+			console.log('Wrong credentials')
+			setMessage(`Wrong username or password`)
+			setMessageType('error')
+			setTimeout(() => {
+				setMessage(null)
+			}, 5000)
+		}
+	}
 
-  const handleLogout = () => {
-    window.localStorage.removeItem('loggedBlogappUser')
-    setUser(null)
-  }
+	const handleLogout = () => {
+		window.localStorage.removeItem('loggedBlogappUser')
+		setUser(null)
+	}
 
-  const loginForm = () => {
-    const hideWhenVisible = { display: loginVisible ? 'none' : '' }
-    const showWhenVisible = { display: loginVisible ? '' : 'none' }
-    
-    return(
-      <div>
-        <div style={hideWhenVisible}>
-          <button onClick={() => setLoginVisible(true)}>Login</button>
-        </div>
+	const loginForm = () => {
+		const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+		const showWhenVisible = { display: loginVisible ? '' : 'none' }
 
-        <div style={showWhenVisible}>
-        <LoginForm
-          username = {username}
-          password = {password}
-          handleUsernameChange = {({ target }) => setUsername(target.value)}
-          handlePasswordChange = {({ target }) => setPassword(target.value)}
-          handleSubmit={handleLogin}
-        />
-        <button onClick={() => setLoginVisible(false)}>cancel</button>
-        </div>
-      </div>
-    )
-  }
+		return (
+			<div>
+				<div style={hideWhenVisible}>
+					<button onClick={() => setLoginVisible(true)}>Login</button>
+				</div>
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      <Notification message={message} type={messageType}/>
-      {user === null ?
-        loginForm() :
-        <div>
-          <p>{user.name} logged-in</p>
-          <button onClick={handleLogout}>logout</button>
-          <BlogForm createBlog={addBlog} />
-          {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} />
-          )}
-        </div>
-      }
-    </div>
-  )
+				<div style={showWhenVisible}>
+					<LoginForm
+						username={username}
+						password={password}
+						handleUsernameChange={({ target }) => setUsername(target.value)}
+						handlePasswordChange={({ target }) => setPassword(target.value)}
+						handleSubmit={handleLogin}
+					/>
+					<button onClick={() => setLoginVisible(false)}>cancel</button>
+				</div>
+			</div>
+		)
+	}
+
+	const blogFormRef = useRef()
+
+	const blogFrom = () => (
+		<Togglable buttonLabel="new blog" ref={blogFormRef}>
+			<BlogForm createBlog={addBlog} />
+		</Togglable>
+	)
+
+	return (
+		<div>
+			<h2>blogs</h2>
+			<Notification message={message} type={messageType} />
+			{user === null ? (
+				loginForm()
+			) : (
+				<div>
+					<p>{user.name} logged-in</p>
+					<button onClick={handleLogout}>logout</button>
+					{blogFrom()}
+					{/* <BlogForm createBlog={addBlog} /> */}
+					{blogs.map((blog) => (
+						<Blog key={blog.id} blog={blog} />
+					))}
+				</div>
+			)}
+		</div>
+	)
 }
 
 export default App
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,35 +1,20 @@
-const LoginForm = ({
-  username,
-  password,
-  handleUsernameChange,
-  handlPasswordChange,
-  handleSubmit
-  }) => {
-  return (
-  <div>
-  <h2>log in to application</h2>
-  <form onSubmit={handleSubmit}>
-    <div>
-      username
-      <input
-        type="text"
-        value={username}
-        name="Username"
-        onChange={handleUsernameChange}
-      />
-    </div>
-    <div>
-      password
-      <input
-        type="password"
-        value={password}
-        name="Password"
-        onChange={handlPasswordChange}
-      />
-    </div>
-    <button type="submit">login</button>
-  </form>
-  </div>
-)}
+const LoginForm = ({ username, password, handleUsernameChange, handlePasswordChange, handleSubmit }) => {
+	return (
+		<div>
+			<h2>log in to application</h2>
+			<form onSubmit={handleSubmit}>
+				<div>
+					username
+					<input type="text" value={username} name="Username" onChange={handleUsernameChange} />
+				</div>
+				<div>
+					password
+					<input type="password" value={password} name="Password" onChange={handlePasswordChange} />
+				</div>
+				<button type="submit">login</button>
+			</form>
+		</div>
+	)
+}
 
 export default LoginForm
