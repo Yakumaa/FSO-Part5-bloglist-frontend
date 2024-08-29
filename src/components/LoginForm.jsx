91107493f18efa96c57a2548cb3b48d5f297ddbc
Yakumaa <maharjanshrish8@gmail.com>
2024-08-29 22:59:19 +0545

    chore: Add id attributes to username and password inputs in LoginForm component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -7,13 +7,15 @@ const LoginForm = ({ username, password, handleUsernameChange, handlePasswordCha
 			<form onSubmit={handleSubmit}>
 				<div>
 					username
-					<input type="text" value={username} name="Username" onChange={handleUsernameChange} />
+					<input id="username" type="text" value={username} name="Username" onChange={handleUsernameChange} />
 				</div>
 				<div>
 					password
-					<input type="password" value={password} name="Password" onChange={handlePasswordChange} />
+					<input id="password" type="password" value={password} name="Password" onChange={handlePasswordChange} />
 				</div>
-				<button type="submit">login</button>
+				<button id="login-button" type="submit">
+					login
+				</button>
 			</form>
 		</div>
 	)
